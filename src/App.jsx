e4a59ce602dfa9f42e17153ddaaaff2b851136e5
Chiaki Obara <obara@ham.works>
2021-04-19 18:16:11 +0900

    完了形のベースをHTML状態で作成

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,42 @@ import React from "react";
 import "./App.css";
 
 export const App = () => {
-	return <div>aaa</div>;
+	return (
+		<>
+			<div className="input-area">
+				<input type="text" placeholder="TODOを入力" />
+				<button>追加</button>
+			</div>
+			<div className="incomplete-area">
+				<p className="title">未完了のTODO</p>
+				<ul>
+					<li>
+						<div className="list-row">
+							<p>あ</p>
+							<button>完了</button>
+							<button>削除</button>
+						</div>
+					</li>
+					<li>
+						<div className="list-row">
+							<p>い</p>
+							<button>完了</button>
+							<button>削除</button>
+						</div>
+					</li>
+				</ul>
+			</div>
+			<div className="complete-area">
+				<p className="title">完了のTODO</p>
+				<ul>
+					<li>
+						<div className="list-row">
+							<p>う</p>
+							<button>戻す</button>
+						</div>
+					</li>
+				</ul>
+			</div>
+		</>
+	);
 }
